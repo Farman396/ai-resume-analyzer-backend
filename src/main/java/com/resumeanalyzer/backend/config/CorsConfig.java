70d603b4e9b5a272c08150fd7abed802bc7131f0
Farman Gour <farmangada59@gmail.com>
2026-05-28 11:33:17 +0530

    Refactor CORS configuration to use WebMvcConfigurer

--- src/main/java/com/resumeanalyzer/backend/config/CorsConfig.java
+++ src/main/java/com/resumeanalyzer/backend/config/CorsConfig.java
@@ -11,35 +11,17 @@ import java.util.List;
 @Configuration 
 public class CorsConfig {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration =
-                new CorsConfiguration();
-
-        configuration.setAllowedOrigins(
-                List.of("https://ai-resume-analyzer-frontend-pro.vercel.app")
-                
-        );
-
-        configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        );
-
-        configuration.setAllowedHeaders(
-                List.of("*")
-        );
-
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration(
-                "/**",
-                configuration
-        );
-
-        return source;
+   @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173", "https://ai-resume-analyzer-frontend-pro.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
