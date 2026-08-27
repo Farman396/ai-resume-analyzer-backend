58566ab508fcdbcfe53339bb63d9ef14e4e73f2e
Farman Gour <farmangada59@gmail.com>
2026-08-27 12:36:25 +0530

    Change API endpoint from '/api/resume' to '/resume'

--- src/main/java/com/resumeanalyzer/backend/controller/ResumeController.java
+++ src/main/java/com/resumeanalyzer/backend/controller/ResumeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("/api/resume")
+@RequestMapping("/resume")
 @RequiredArgsConstructor
 @CrossOrigin("*")
 public class ResumeController {
